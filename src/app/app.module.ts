2d761adf6d58d3126ce65fc810620c8c24d6f725
Alejandro Pachas <alejandromydev@gmail.com>
2024-04-09 20:25:08 -0500

    [app.module.ts] file edited

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterContentComponent } from './public/components/footer-content/footer-content.component';
+import { SideNavigationBarComponent } from './public/components/side-navigation-bar/side-navigation-bar.component';
+import { SourceListItemComponent } from './news/components/source-list-item/source-list-item.component';
+import { LanguageSwitcherComponent } from './public/components/language-switcher/language-switcher.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterContentComponent,
+    SideNavigationBarComponent,
+    SourceListItemComponent,
+    LanguageSwitcherComponent
   ],
   imports: [
     BrowserModule,
